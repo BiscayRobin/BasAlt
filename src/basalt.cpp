2e188d05e4c2afc8857b426582827384c52ec533
BiscayRobin <biscay.rob@gmail.com>
2020-10-27 19:25:56 +0100

    adding lexer step to the main job

--- src/basalt.cpp
+++ src/basalt.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "../libs/CLI11.hpp"
+#include "lexer.hpp"
+#include "token.hpp"
 
 int main(int argc, char **argv) {
     CLI::App app{"Interpreter for a BASIC Alternative"};
@@ -14,6 +16,18 @@ int main(int argc, char **argv) {
     } catch (const CLI::ParseError &e) {
         return app.exit(e);
     }
+
+    std::ifstream file;
+    file.open(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Could not open file: " << filename << "." << std::endl;
+    }
+
+    // lex it
+    auto tokens = lex(file);
+
+    file.close();
 
     return 0;
 }
