7f05969fc1385d247487990a202e919cc4f7d116
BiscayRobin <biscay.rob@gmail.com>
2020-11-16 13:51:40 +0100

    ostream operator and rework of constructors

--- src/token.cpp
+++ src/token.cpp
@@ -2,53 +2,70 @@
 
 using namespace std;
 
-Token::Token(TokenType type) { this->tag = type; }
+Token::Token(TkType type) : tag(type) {}
 
-Token::Token(TokenType type, double nb) {
-    this->tag = type;
-    this->number = nb;
-}
+Token::Token(TkType type, double nb) : tag(type), number(nb) {}
 
-Token::Token(TokenType type, string str) {
-    this->tag = type;
-    this->text = str;
-}
+Token::Token(TkType type, string str) : tag(type), text(str) {}
 
-Token& Token::operator=(const Token& elt) {
-    if ((tag == TokenType::string_lit || tag == TokenType::variable) &&
-        (elt.tag == TokenType::string_lit || elt.tag == TokenType::variable)) {
+Token &Token::operator=(const Token &elt) {
+    if ((tag == TkType::string_lit || tag == TkType::variable) &&
+        (elt.tag == TkType::string_lit || elt.tag == TkType::variable)) {
         text = elt.text;
         return *this;
     }
-    if (tag == TokenType::string_lit) {
+    if (tag == TkType::string_lit || tag == TkType::variable) {
         text.~string();
     }
 
     switch (elt.tag) {
-        case TokenType::number:
-            number = elt.number;
-            break;
-        case TokenType::variable:
-        case TokenType::string_lit:
-            new (&text) string(elt.text);
-            break;
-        default:
-            break;
+    case TkType::number_lit:
+        number = elt.number;
+        break;
+    case TkType::variable:
+    case TkType::string_lit:
+        new (&text) string(elt.text);
+        break;
+    default:
+        break;
     }
     tag = elt.tag;
     return *this;
 }
 
-Token::Token(const Token& elt) {
-	tag = elt.tag;
-	if(tag == TokenType::string_lit || tag == TokenType::variable) {
-		new (&text) string(elt.text);
-	}
-	if(tag == TokenType::number) {
-		number = elt.number;
-	}
+Token::Token(const Token &elt) {
+    tag = elt.tag;
+    if (tag == TkType::string_lit || tag == TkType::variable) {
+        new (&text) string(elt.text);
+    }
+    if (tag == TkType::number_lit) {
+        number = elt.number;
+    }
 }
 
 Token::~Token() {
-    if (tag == TokenType::string_lit) text.~string();
+    if (tag == TkType::string_lit || tag == TkType::variable) {
+        text.~string();
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Token &tk) {
+    static const std::string
+        TOKEN_NAMES[static_cast<int>(Token::TkType::ENUM_SIZE)] = {
+            [static_cast<int>(Token::TkType::number_lit)] = "num",
+            [static_cast<int>(Token::TkType::plus)] = "plus",
+            [static_cast<int>(Token::TkType::string_lit)] = "str",
+            [static_cast<int>(Token::TkType::variable)] = "var",
+        };
+    os << "<Tk: " << TOKEN_NAMES[static_cast<int>(tk.tag)] << " >";
+
+    if (tk.tag == Token::TkType::number_lit) {
+        os.precision(17);
+        os << '(' << tk.number << ')';
+    }
+    if (tk.tag == Token::TkType::variable ||
+        tk.tag == Token::TkType::string_lit) {
+        os << '(' << tk.text << ')';
+    }
+    return os;
 }
